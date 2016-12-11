3d313a7d1caa58bf3c7dcdd33e2532a8137c0325
Marco <Marco@MBP-di-User.homenet.telecomitalia.it>
2016-12-11 20:06:13 +0100

    sync method

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | This file is where you may define all of the routes that are handled
+  | by your application. Just tell Laravel the URIs it should respond
+  | to using a Closure or controller method. Build something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'api/v1'], function () {
+
+    Route::get('sync', function () {
+        return response()->json([
+                    'timestamp' => time()
+        ]);
+    });
 });
